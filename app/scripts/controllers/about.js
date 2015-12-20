c73d49bc10819cb84d239fc02761547a589a211a
nony1992 <ionutvalentin66@gmail.com>
2015-12-20 13:53:44 +0200

    Scaffolding the app yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name templateStoreApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the templateStoreApp
+ */
+angular.module('templateStoreApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
